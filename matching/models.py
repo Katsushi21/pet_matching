2f876931edba5b1ead649c3d83e1f612c988a3a5
Katsushi21 <sheoaspendie@gmail.com>
2020-12-14 21:25:09 +0900

    db ok

--- matching/models.py
+++ matching/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Dog_Data:
     """犬のデータ管理モデル"""
     GENDER = (('M', 'Male'), ('F', 'Female'))
-    WALKING_ON_LEASH = (('Y', 'Yes'), ('N', 'No'))
+    YES_OR_NO = (('1', 'Yes'), ('0', 'No'))
 
     name = models.CharField(max_length=30, verbose_name='name')
     gender = models.CharField(max_length=6, choices=GENDER, verbose_name='gender')
@@ -17,4 +17,5 @@ class Dog_Data:
     color = models.CharField(max_length=20, verbose_name='color')
     hair = models.CharField(max_length=10, verbose_name='hair')
     reason_for_arrival = models.TextField(null=True, verbose_name='reason_for_arrival')
-    walking_on_leash = models.CharField(max_length=3, choices=WALKING_ON_LEASH, verbose_name='walking_on_leash')
+    walking_on_leash = models.CharField(max_length=3, choices=YES_OR_NO, verbose_name='walking_on_leash')
+    can_live_with_dogs = models.CharField(max_length=3, choices=YES_OR_NO, verbose_name='can_live_with_dogs')
